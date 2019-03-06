261a8d125e1a02632c8f4e13b6e9edc0020f1c7b
wjn <2982208622@qq.com>
2019-03-06 11:06:48 +0800

    初始化

---
+++ src/App.vue
@@ -0,0 +1,43 @@
+<template>
+  <div id="app">
+    <v-header :seller="seller"/>
+  </div>
+</template>
+
+<script>
+import VHeader from 'components/v-header/v-header'
+import { getSeller } from 'api'
+
+export default {
+  name: 'app',
+  data () {
+    return {
+      seller: {}
+    }
+  },
+  components: {
+    VHeader
+  },
+  created() {
+    this._getSeller()
+  },
+  methods: {
+    _getSeller() {
+      getSeller({
+        id: this.seller.id
+      }).then((seller) => {
+        this.seller = Object.assign({}, this.seller, seller)
+      })
+    }
+  }
+}
+</script>
+<style lang="stylus">
+#app
+  font-family 'Avenir', Helvetica, Arial, sans-serif
+  -webkit-font-smoothing antialiased
+  -moz-osx-font-smoothing grayscale
+  text-align center
+  color #2c3e50
+  margin-top 60px
+</style>
